89e280335e8e2b8912e9f474421f412e3bc59594
Gloria Lim <glorialimartt@gmail.com>
2023-09-19 19:43:20 -0400

    Initial commit head.js

---
+++ head.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
+  }
+  else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
+  }
+};
+
+
+// Create a function head which returns the first item in the array.
+const head = function(array) {
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
