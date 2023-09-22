dafe506637fc6d9b7442806e46b08e56452de84a
Gloria Lim <glorialimartt@gmail.com>
2023-09-21 22:15:18 -0400

    Initial commit without.js

---
+++ without.js
@@ -0,0 +1,37 @@
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!source[i].includes(itemsToRemove)) {
+      result.push(source[i]);
+    }
+  }
+}
+
+const assertArraysEqual = function(source, result) {
+  const eqArrays = function(source, result) {
+    if (source.length === result.length) {
+      for (let i = 0; i < source.length; i++) {
+        let perfectMatch = false;
+        if (source[i] === result[i]) {
+          perfectMatch = true;
+        } else {
+          perfectMatch = false;
+          break;
+        }
+        return perfectMatch;
+      }
+    }
+  };
+
+  if (eqArrays(source, result) === true) {
+    console.log(`✅✅✅ Arrays Equal Assertion Passed: ${source} === ${result}`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Arrays Equal Assertion Failed: ${source} !== ${result}`);
+  }
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
