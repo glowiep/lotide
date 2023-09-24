544827ab642c91a7d6c39c7acdb234899fd8257c
Gloria Lim <glorialimartt@gmail.com>
2023-09-24 16:42:28 -0400

    Modified countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -38,4 +38,3 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
-assertEqual(result1, { Fang: 2, Jason: 1 });
