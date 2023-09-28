682d364314368ddff625e91fa5683c4903371bdd
Gloria Lim <glorialimartt@gmail.com>
2023-09-28 00:37:15 -0400

    Initial commit findKey.js

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const keysArray = Object.keys(object);
+  for (let key of keysArray) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+                
+console.log(result1);
+
+// Assertion Tests
+assertEqual(result1, "noma"); // true
