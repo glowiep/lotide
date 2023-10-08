98a1768c37e7028efdc2d88c32b74d40c7289d8e
Gloria Lim <glorialimartt@gmail.com>
2023-10-08 14:20:47 -0400

    Used recursion to allow eqArrays to support sub-arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,21 +7,41 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      let perfectMatch = false;
-      if (arr1[i] === arr2[i]) {
-        perfectMatch = true;
-      } else {
-        perfectMatch = false;
-        break;
+  // Return FALSE if array lenghts are not equal
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // If array lengths are equal, iterate over items in array
+  for (let i = 0; i < arr1.length; i++) {
+    // Recursive case to support sub-arrays check
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      const result = eqArrays(arr1[i], arr2[i]);
+      // Return FALSE if it fails the eqArrays check
+      if (!result) {
+        return false;
       }
-      return perfectMatch;
+    } else if (arr1[i] !== arr2[i]) {
+      return false; // Return FALSE if the array values are not equal
     }
   }
+  return true;
+  
 };
 
 // Test assertions
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+
+console.log("-----------");
+assertEqual(eqArrays([1, [2, [3]]], [1, [2, [3]]]), true); // True
+assertEqual(eqArrays([1, [2, [3]]], [1, [2, 3]]), false); // False
+assertEqual(eqArrays([[], [], [[[3, [4, 5, [6]]]]]], [[], [], [[[3, [4, 5, [6]]]]]]), true); // True
+assertEqual(eqArrays([[], [], [[[3, [4, 5, 6]]]]], [[], [], [[[3, [4, 5, [6]]]]]]), false); // False
+assertEqual(eqArrays([[], [], [[]]], [[], [], [[]]]), true); // True
+assertEqual(eqArrays([[], [], [[]]], [[], [], []]), false); // False
+console.log("-----------");
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // True
 assertEqual(eqArrays(["1", "a", 3], ["1", "a", 3]), true); // True
 assertEqual(eqArrays([null, [], 0], [null, [], 0]), true); // True
