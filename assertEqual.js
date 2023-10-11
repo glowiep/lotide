2099e166222cc72f01b1a14ccc7ca757e7541e18
Gloria Lim <glorialimartt@gmail.com>
2023-10-11 16:21:57 -0400

    Added comment

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+// Function that takes in 2 values and returns true if they are equal and false otherwise.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
