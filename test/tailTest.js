9b51653e15b992750e0fcc49be50fc245ef69e5c
Gloria Lim <glorialimartt@gmail.com>
2023-10-11 23:50:49 -0400

    Refactored with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,17 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const tail = require('../tail');
-// Tests: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log("Using assertEqual:");
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-console.log("\nUsing assertArraysEqual:");
-assertArraysEqual(tail([1, "Hello", "Lighthouse", "Labs"]), ["Hello", "Lighthouse", "Labs"])
-// Empty array or one element as input to tail function should return an empty array
-assertArraysEqual(tail([]), []);
-assertArraysEqual(tail(["One"]), []);
+describe('#tail', () => {
+  it("returns ['Lighthouse, 'Labs] for ['Hello', 'Lighthouse, 'Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [] for ['One']", () => {
+    assert.deepEqual(tail(["One"]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
