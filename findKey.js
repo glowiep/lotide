3db16f94177094837d211636a9ece05ad9836382
Gloria Lim <glorialimartt@gmail.com>
2023-10-13 16:43:51 -0400

    Moved tests to test file

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // Function that takes in an object and callback and returns the first key that meets the criteria specified in callback.
 const findKey = function(object, callback) {
   const keysArray = Object.keys(object);
@@ -17,17 +9,3 @@ const findKey = function(object, callback) {
 };
 
 module.exports = findKey;
-
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma"
-                
-console.log(result1);
-
-// Assertion Tests
-assertEqual(result1, "noma"); // true
---
+++ test/findKeyTest.js
@@ -0,0 +1,16 @@
+const assertEqual = require('../assertEqual');
+const findKey = require('../findKey');
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+                
+console.log(result1);
+
+// Assertion Tests
+assertEqual(result1, "noma"); // true
