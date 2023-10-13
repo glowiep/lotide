e37fa354449f585f3224360f61fc6f624cb5d019
Gloria Lim <glorialimartt@gmail.com>
2023-10-13 02:44:53 -0400

    Commented out tests

--- map.js
+++ map.js
@@ -1,6 +1,4 @@
 // Function that takes in an array and a callback and returns a new array based on the results of the callback on each item in the array.
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -11,9 +9,13 @@ const map = function(array, callback) {
 
 module.exports = map;
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
+/*
+// Test
+const words = ["ground", "control", "to", "major", "tom"];
+// Import assertArraysEqual function 
 const assertArraysEqual = require('./assertArraysEqual')
 
+const results1 = map(words, word => word[0]);
+
 assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+*/
