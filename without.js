c68b5514b407e3a66dbbac8b7a063f12c617a626
Gloria Lim <glorialimartt@gmail.com>
2023-10-13 16:36:20 -0400

    Removed commented out tests

--- without.js
+++ without.js
@@ -9,13 +9,3 @@ const without = function(source, itemsToRemove) {
 };
 
 module.exports = without;
-
-//     Import assertArraysEqual function
-// const assertArraysEqual = require('./assertArraysEqual')
-
-//     Test assertions
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-
-//     Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
